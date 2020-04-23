e44547ee05bee1c4834582e95ec8a8a8f6443747
Jerome <jeromej.enriquez@gmail.com>
2020-04-22 20:38:50 -0700

    4.22.20.8.38 -Minor fixes to code

--- Classroomverse.py
+++ Classroomverse.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from _ast import Lambda
+
 
 def main():
     root = Tk()
@@ -7,23 +7,30 @@ def main():
     root.geometry('853x480')
     root.configure(bg='white')
 
-    def Main_shows_people():
-        random = Label(main, text="These are people.")
-        random.pack()
+    def show_home_page():
+        HomeLabel = Label(main, text="This is the homepage.")
+        HomeLabel.pack()
 
-    #def Main_shows_assignments():
+    def show_people():
+        PeopleLabel = Label(main, text="These are people.")
+        PeopleLabel.pack()
 
-    #def Main_shows_grades():
+    def shows_assignments():
+        AssignmentsLabel = Label(main, text="These are assignments.")
+        AssignmentsLabel.pack()
+    def shows_grades():
+        GradesLabel = Label(main, text="These are grades.")
+        GradesLabel.pack()
 
     title = Label(root, text="Classroomverse", font=("Tahoma", 26), bg="white", padx=3, pady=3)
     title.pack(side="top")
     sidebar = Frame(root, width=200, bg='#CCF7FF', borderwidth=1)
     sidebar.pack(expand=False, fill='both', side='left', anchor='nw')
 
-    button1 = Button(sidebar, text="Home", padx=10, pady=10, command=Main_shows_people, relief=GROOVE, bg='#CCF7FF')
-    button2 = Button(sidebar, text="Assignments", padx=10, pady=10, command=Main_shows_people, relief=GROOVE, bg='#CCF7FF')
-    button3 = Button(sidebar, text="Grades", padx=10, pady=10, command=Main_shows_people, relief=GROOVE, bg='#CCF7FF')
-    button4 = Button(sidebar, text="People", padx=10, pady=10, command=Main_shows_people, relief=GROOVE, bg='#CCF7FF')
+    button1 = Button(sidebar, text="Home", padx=10, pady=10, command=show_home_page, relief=GROOVE, bg='#CCF7FF')
+    button2 = Button(sidebar, text="Assignments", padx=10, pady=10, command=shows_assignments, relief=GROOVE, bg='#CCF7FF')
+    button3 = Button(sidebar, text="Grades", padx=10, pady=10, command=shows_grades, relief=GROOVE, bg='#CCF7FF')
+    button4 = Button(sidebar, text="People", padx=10, pady=10, command=show_people, relief=GROOVE, bg='#CCF7FF')
     button1.pack(fill=X)
     button2.pack(fill=X)
     button3.pack(fill=X)
