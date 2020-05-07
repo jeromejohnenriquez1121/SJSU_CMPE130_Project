89488a2e9c08b050f9c2a1ff6fbb9048eea2fc9b
Saima Yunus <saimayunus925@gmail.com>
2020-05-07 16:08:35 -0700

    finished rough quicksort function

--- school_content.py
+++ school_content.py
@@ -5,8 +5,14 @@ of course, represents an assignment; it has a title, a due date, a total amount 
 possible points, and the total amount of points earned. The grade is found by dividing
 the points earned by the total points possible on the assignment and multiplying this by 100.
 The latter represents a student; the student has a full name, an email address, an ID 
-number, and a list of assignments. The students can be sorted in different ways, whether
-it be by their names, their ID numbers, or their grades. 
+number, and a list of assignments. The students can be sorted by their names or their 
+ID numbers; their grades can also be sorted from lowest to highest, in order to easily
+get each student's grade quartiles and average grade. 
+
+We will sort the student's assignments by grade using the sort_grades() function; the algorithm 
+used will be quicksort. We need a swap() function and an overloaded less-than operator to do this. 
+The less-than operator will be in the 'assignment' class; the swap() function will be in the 
+'student' class.
 '''
 class assignment:
     def __init__(self, assignment_title, due_date, total_points, points_earned):
@@ -18,6 +24,14 @@ class assignment:
     
     def get_grade(self):
         return self.grade 
+
+    def __lt__(self, new_assignment):
+        # tests if the current assignment has a lower grade
+        # than new_assignment
+        if (self.grade < new_assignment.grade):
+            return True
+        else:
+            return False 
     
     def print_assignment(self):
         print("Assignment: " + self.assignment_title)
@@ -41,18 +55,20 @@ class student:
         new_assignment = assignment(t, d, total, pts) 
         self.assignments.append(new_assignment) 
 
-    '''
-    We will sort the student's assignments by grade using quicksort. We need a swap() function
-    and an overloaded greater-than operator to do this. The greater-than operator will be in
-    the 'assignment' class. 
-    '''
-
     def swap(self, n1, n2):
         # swaps assignments[n1] with assignments[n2]
         self.assignments[n1], self.assignments[n2] = self.assignments[n2], self.assignments[n1] 
 
     def partition(self, l, h):
-        pass 
+        pivot = self.assignments[h]
+        i = l - 1
+        for j in range(l, h):
+            if (self.assignments[j] < pivot):
+                i += 1
+                self.swap(self.assignments[i], self.assignments[j]) 
+                
+        self.swap(self.assignments[i+1], self.assignments[h]) 
+        return i + 1
 
     def sort_grades(self, l, h):
         '''
