9823e391c68bdea82ea6640ddc4dece366e061f6
Saima Yunus <saimayunus925@gmail.com>
2020-05-08 15:18:02 -0700

    quicksort finally works

--- school_content.py
+++ school_content.py
@@ -15,17 +15,28 @@ class QuickSort:
     # 'collection': the array to be sorted
     def partition(self, collection, lo, hi):
         # hi = len(collection) - 1
-        j = self.lo # collection[j] and everything behind it must be less than pivot
-        pivot = self.hi # selecting last item as pivot
+        j = lo # collection[j] and everything behind it must be less than pivot
+        pivot = hi # selecting last item as pivot
         # what do we know? We know that we must go from lo to hi, until we find an item less than the pivot
         # we use i and j for this, i is in the for loop, j is the index for most recent element < pivot element
         for i in range(lo, hi):
-            if (self.collection[i] < self.collection[j]):
-                
-        # when collection[i] < collection[pivot], we've found a left partition item
-        # we swap collections[i] and collections[j], so now j is the first element < pivot
-        # after the for-loop, pivot should be in its correct position
-        # all items to its left must be less than it, al items to its right must be greater than it
+            # when collection[i] < collection[pivot], we've found a left partition item
+            # we swap collections[i] and collections[j], so now j is the first element < pivot   
+            # we increment j to move on to find the next element < pivot        
+            if (collection[i] < collection[pivot]): 
+                collection[i], collection[j] = collection[j], collection[i]
+                j += 1
+        # now we swap our 'marker' j with the pivot
+        collection[pivot], collection[j] = collection[j], collection[pivot] 
+        return j 
+
+
+    def quicksort(self, collection, lo, hi):
+        # hi = len(collection) - 1
+        if (lo < hi):
+            pivot = self.partition(collection, lo, hi)
+            self.quicksort(collection, lo, pivot-1)
+            self.quicksort(collection, pivot+1, hi) 
 
 
 class student: 
@@ -43,11 +54,11 @@ class student:
         letter_grade = pts / total 
         self.grades.append(letter_grade) 
 
-    # TODO: selection sort 
-    # calls selection_sort function from SelectionSort class
+    # TODO: quick sort 
+    # calls quicksort function from SelectionSort class
     def sort_grades(self):
         n = len(self.grades) 
-        # QuickSort().quicksort(self.grades, 0, n) 
+        QuickSort().quicksort(self.grades, 0, n-1)  
  
     # TODO: calculate grade quartiles and avg grade
 
--- test.py
+++ test.py
@@ -6,29 +6,11 @@ from school_content import student, QuickSort
 
 # code 
 
-def partition(collection, low, high):
-    # high = len-1
-    j = low # collection[j] and every element before it is < pivot
-    pivot = high
-
-    for i in range(low, high):
-        if (collection[i] < collection[pivot]):
-            collection[i], collection[j] = collection[j], collection[i] 
-            j += 1
-    collection[pivot], collection[j] = collection[j], collection[pivot]
-    return j 
-    
-def quicksort(collection, low, high):
-    # high = len-1
-    if (low < high):
-        pivot = partition(collection, low, high)
-        quicksort(collection, low, pivot-1)
-        quicksort(collection, pivot+1, high) 
-
-
 newlist = [0, 9, 3, 7, 6, 1, 8, 2, 5, 4] # list to test our algorithm on 
-n = len(newlist)
+n = len(newlist) 
 
-quicksort(newlist, 0, n-1) 
+qs = QuickSort() 
 
-print(newlist)  
+qs.quicksort(newlist, 0, n-1)   
+
+print(newlist) 
