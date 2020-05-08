0a7bb6e7403355311bdb96da1f003556f966b061
Saima Yunus <saimayunus925@gmail.com>
2020-05-08 13:52:26 -0700

    decided to use selection sort instead of quick sort

--- school_content.py
+++ school_content.py
@@ -5,13 +5,13 @@ address, an ID number, and a list of grades. The students can be sorted by their
 or their ID numbers; their grades can also be sorted from lowest to highest, in order to 
 easily get each student's grade quartiles and average grade. 
 
-We will sort the student's grades using a specialized sorting class; the algorithm 
-used will be quicksort. 
-'''     
+We will sort each student's grades with the selection sort algorithm; the specialized selectionsort() 
+class will take in any array of numbers and sort them. The sort_grades() function in our
+student class will make use of this function.
+'''
 
-class QuickSort:
+class SelectionSort:
     pass 
-
 
 class student: 
     def __init__(self, name, student_id, email, grades=[]): 
@@ -22,29 +22,18 @@ class student:
     
     def add_grade(self): 
         # adds a grade to the 'grades' list
+        print("--------------") 
         total = float(input("Total Points Possible on Assignment: "))
         pts = float(input("Points Earned: ")) 
         letter_grade = pts / total
         self.grades.append(letter_grade) 
 
-    def swap(self, n1, n2):
-        # swaps assignments[n1] with assignments[n2]
-        temp = self.grades[n1]
-        self.grades[n1] = self.grades[n2]
-        self.grades[n2] = temp 
-
-    def partition(self, l, h):
-        pass 
-
-    def sort_grades(self, l, h):
-        '''
-        This function sorts the student's grades from lowest to highest. 
-        Their grades are stored in the "grades" list. The algorithm used 
-        will be quick sort, so we'll use the QuickSort class in the function.
-        l: lowest index in array, h: highest index in array
-        '''
-        pass  
-
+    # TODO: quick sort
+    # calls quicksort function from QuickSort class
+    def sort_grades(self):
+        n = len(self.grades) 
+        QuickSort().quicksort(self.grades, 0, n) 
+ 
     # TODO: calculate grade quartiles and avg grade
 
     def get_grade_quartiles(self):
@@ -54,3 +43,7 @@ class student:
     def get_average(self):
         # get average grade
         pass 
+
+
+
+
--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-from school_content import *
-
 # test code for students
 
 # we want to see if our quicksort function works
 
-saima = student("Saima Yunus", "012257752", "saima.yunus@sjsu.edu") 
-
-
-n2 = int(input("# OF ASSIGNMENTS: "))
-for j in range(n2):
-    saima.add_grade()
- 
-
-saima.sort_grades(0, n2) 
-
-print(saima.grades) 
-
-
+from school_content import * 
