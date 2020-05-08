7a2e621087faa28ac249442629cd61458e691faa
Saima Yunus <saimayunus925@gmail.com>
2020-05-08 14:31:37 -0700

    few more modifications

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 # we want to see if our quicksort function works
 
-from school_content import student
+from school_content import student, QuickSort 
 
 # code 
+
+newlist = [0, 9, 3, 7, 6, 1, 8, 2, 5, 4] # list to test our algorithm on 
