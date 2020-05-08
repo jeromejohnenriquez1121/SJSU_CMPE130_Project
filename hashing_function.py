856edf928fa1bce8056a46c966f6443196a4b1b6
Rushi Patel <rushi121099@gmail.com>
2020-05-08 15:00:42 -0700

    fixes

--- hashing_function.py
+++ hashing_function.py
@@ -8,14 +8,14 @@ def make_student():
     newStudent['name'] = input('Please enter Student Name: ')
     while(1):
         newStudent['student_id'] = input('Please enter Student ID: ')
-        if(len(newStudent['student_id']) == 9):
+        if len(newStudent['student_id']) == 9:
             break
         else:
             print("Invalid Student ID entered. Please try again.")
 
     newStudent['email'] = input('Please give Student Email: ')
 
-    print("\n Student", newStudent['name'], "created !")
+    print("\n Student", newStudent['name'], "created.")
     s = student(newStudent['name'], newStudent['student_id'], newStudent['email'], " ")
     return s
 
@@ -28,17 +28,23 @@ class StudentHashTable:
         print("Inserting Student")
         newStudent = make_student()
         sID = newStudent.student_id
-        indexNumber = int(sID[6:8]) % len(self.studentList)
+        indexNumber = int(sID[6:9]) % len(self.studentList)
 
-        if(self.studentList[indexNumber] == "NULL"):
+        for e in self.studentList:
+            if e != "NULL" and e.email == newStudent.email:
+                print("Student with the same email already exist. Please Try again")
+                return
+
+        if self.studentList[indexNumber] == "NULL":
             self.studentList[indexNumber] = newStudent
             print(newStudent.name, "added to the table.")
         else:
             while(1):
-                if(self.studentList[indexNumber].student_id == newStudent.student_id or self.studentList[indexNumber].email == newStudent.email):
+                if self.studentList[indexNumber].student_id == newStudent.student_id or \
+                        self.studentList[indexNumber].email == newStudent.email:
                     print("Student with this ID or Email is already added! Cannot make duplicate entry.\n\n")
                     break
-                elif(self.studentList[indexNumber] == "NULL"):
+                elif self.studentList[indexNumber] == "NULL":
                     self.studentList[indexNumber] = student
                     print(newStudent.name, "added to the table\n\n")
                     break
@@ -47,12 +53,12 @@ class StudentHashTable:
     def getStudentInfo(self, sID):
         indexNumber = int(sID[6:8]) % len(self.studentList)
 
-        if(self.studentList[indexNumber].student_id == sID):
+        if self.studentList[indexNumber].student_id == sID:
             print(self.studentList[indexNumber])
             return
         else:
-            while(indexNumber != len(self.studentList) and self.studentList[indexNumber] != "NULL"):
-                if(self.studentList[indexNumber].student_id == sID):
+            while indexNumber != len(self.studentList) and self.studentList[indexNumber] != "NULL":
+                if self.studentList[indexNumber].student_id == sID:
                     print(self.studentList[indexNumber])
                     return
                 indexNumber = (indexNumber + 1) % len(self.studentList)
