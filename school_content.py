5566785ab08ee893fe31da03024ae1ccde709f8a
Rushi Patel <37125645+rushipatel99@users.noreply.github.com>
2020-05-08 23:10:24 -0700

    Update school_content.py

--- school_content.py
+++ school_content.py
@@ -9,6 +9,7 @@ class will take in any array of numbers and sort them. The sort_grades() functio
 student class will make use of this function.
 '''
 
+
 def get_median_index(array, lo, hi):
     # returns median of 'array'
     # lo: index 0 of 'array'
@@ -18,25 +19,17 @@ def get_median_index(array, lo, hi):
     return n2 + lo
 
 
-
-class QuickSort: 
-
+class QuickSort:
     # only two functions needed: partition and quicksort
     # 'collection': the array to be sorted
     def partition(self, collection, lo, hi):
         # hi = len(collection) - 1
-
         j = lo  # collection[j] and everything behind it must be less than pivot
         pivot = hi  # selecting last item as pivot
-=======
-        j = lo # collection[j] and everything behind it must be less than pivot
-        pivot = hi # selecting last item as pivot
-
         # what do we know? We know that we must go from lo to hi, until we find an item less than the pivot
         # we use i and j for this, i is in the for loop, j is the index for most recent element < pivot element
         for i in range(lo, hi):
             # when collection[i] < collection[pivot], we've found a left partition item
-
             # we swap collections[i] and collections[j], so now j is the first element < pivot
             # we increment j to move on to find the next element < pivot
             if (collection[i] < collection[pivot]):
@@ -45,38 +38,17 @@ class QuickSort:
         # now we swap our 'marker' j with the pivot
         collection[pivot], collection[j] = collection[j], collection[pivot]
         return j
-=======
-            # we swap collections[i] and collections[j], so now j is the first element < pivot   
-            # we increment j to move on to find the next element < pivot        
-            if (collection[i] < collection[pivot]): 
-                collection[i], collection[j] = collection[j], collection[i]
-                j += 1
-        # now we swap our 'marker' j with the pivot
-        collection[pivot], collection[j] = collection[j], collection[pivot] 
-        return j 
-
-
-
 
     def quicksort(self, collection, lo, hi):
         # hi = len(collection) - 1
         if (lo < hi):
             pivot = self.partition(collection, lo, hi)
-
             self.quicksort(collection, lo, pivot - 1)
             self.quicksort(collection, pivot + 1, hi)
 
 
-class student: 
-    def __init__(self, name, student_id, email, grades=[], avg=0, quartiles=[], completed = False):
-        self.name = name
-        self.student_id = student_id
-        self.email = email
-        self.grades = grades
-        self.avg = avg
-        self.quartiles = quartiles
-        self.completed = completed
-    
+class student:
+
     def __str__(self):
         ret = "name:\t" + self.name + "\n"
         ret += "student ID:\t" + self.student_id + "\n"
@@ -90,6 +62,15 @@ class student:
             print("*** Grades calculations have not been made for this student yet ***")
         return ret
 
+    def __init__(self, name, student_id, email, grades=[], avg=0, quartiles=[], completed = False):
+        self.name = name
+        self.student_id = student_id
+        self.email = email
+        self.grades = grades
+        self.avg = avg
+        self.quartiles = quartiles
+        self.completed = completed
+
     def add_grade(self):
         # adds a grade to the 'grades' list
         print("--------------")
@@ -99,6 +80,7 @@ class student:
         self.grades.append(letter_grade)
 
         # calls quicksort function from QuickSort class
+
     def sort_grades(self):
         n = len(self.grades)
         QuickSort().quicksort(self.grades, 0, n - 1)
@@ -107,15 +89,15 @@ class student:
         # calculate grade quartiles with global median function
         # Q1 (lower quartile): median of lower half
         # Q2: median
-        # Q3 (upper quartile): median of upper half 
+        # Q3 (upper quartile): median of upper half
         q1 = 0
         q2 = 0
-        q3 = 0         
-        n1 = len(self.grades) 
+        q3 = 0
+        n1 = len(self.grades)
         # 1. make sure array is sorted in ascending order
-        self.sort_grades() 
+        self.sort_grades()
         # 2. find median of array to divide it in lower and upper half
-        index_mid = get_median_index(self.grades, 0, n1) 
+        index_mid = get_median_index(self.grades, 0, n1)
         # for [65, 70, 80, 90], index_mid returned 2, i.e. the index of 80
         # this happened when I tested an 8-element-long list in IDLE too
         # for even numbers, get_median_index() returns the index of the second median #
@@ -132,21 +114,10 @@ class student:
         n2 = len(lower_half)
         upper_half = self.grades[index_mid:]
         n3 = len(upper_half)
-        # we will use same odd array length vs. even array length process for each half of the array
-        # that we used for the whole array
-   
-        # now to find q1 and q3 
-        # q1: median of lower half
-        # q3: median of upper half
-        lower_half = self.grades[0:index_mid]
-        n2 = len(lower_half) 
-        upper_half = self.grades[index_mid:] 
-        n3 = len(upper_half) 
-        
-        # we will use same odd array length vs. even array length process for each half of the array
-        # that we used for the whole array
-        indexmid_2 = get_median_index(lower_half, 0, n2) # median index of lower half
 
+        # we will use same odd array length vs. even array length process for each half of the array
+        # that we used for the whole array
+        indexmid_2 = get_median_index(lower_half, 0, n2)  # median index of lower half
 
         if (n2 % 2 == 0):
             num1 = lower_half[indexmid_2 - 1]
@@ -154,9 +125,8 @@ class student:
             q1 = (num1 + num2) / 2
         else:
             q1 = lower_half[indexmid_2]
-            
 
-          indexmid_3 = get_median_index(upper_half, 0, n3)
+        indexmid_3 = get_median_index(upper_half, 0, n3)  # median index of upper half
 
         if (n3 % 2 == 0):
             num1 = upper_half[indexmid_3 - 1]
@@ -170,7 +140,6 @@ class student:
 
     def get_quartiles(self):
         return self.quartiles
-
 
     def calculate_average(self):
         # get average grade
